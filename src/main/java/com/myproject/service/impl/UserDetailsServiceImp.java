e85a705ccdf70d71f3505f9c74768981c5724d5d
Vlad <wald.pavliuk@icloud.com>
2019-08-14 16:47:21 +0300

    delete unused imports

--- src/main/java/com/myproject/service/impl/UserDetailsServiceImp.java
+++ src/main/java/com/myproject/service/impl/UserDetailsServiceImp.java
@@ -1,16 +1,10 @@
 package com.myproject.service.impl;
 
-import com.myproject.entity.User;
-import com.myproject.entity.enums.UserRole;
 import com.myproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.util.Optional;
 
 public class UserDetailsServiceImp implements UserDetailsService {
 
